d356dd162b5ab87ce480fbc557b0fe2ddb74792f
Xavier Talpe <xaviertalpe+pro@gmail.com>
2014-05-25 18:55:13 +0200

    attempt to use heroku scheduler tasks

---
+++ bin/query_ticker.js
@@ -0,0 +1,50 @@
+var KrakenApi = require( 'kraken-api' );
+var Mongo = require( 'mongodb' );
+
+var krakenApi = new KrakenApi( 'api_key', 'api_secret' );
+
+krakenApi.api( 'Ticker', {'pair': 'XLTCZEUR'}, function ( error, data ) {
+  if ( error ) {
+    console.log( 'Error retrieving ticker data: ' + error );
+  }
+  else {
+    var tick = parseData( data );
+    saveData( tick );
+  }
+} );
+
+function parseData( data ) {
+  var query_results = data.result;
+
+  var tick_ltc_eur = query_results[ 'XLTCZEUR' ];
+
+  var tick = {};
+  tick.lowest_ask = parseFloat( tick_ltc_eur[ 'a' ][ 0 ] );
+  tick.highest_bid = parseFloat( tick_ltc_eur[ 'b' ][ 0 ] );
+  tick.last_trade = parseFloat( tick_ltc_eur[ 'c' ][ 0 ] );
+  tick.volume_traded = parseFloat( tick_ltc_eur[ 'v' ][ 1 ] );
+  tick.avg_price = parseFloat( tick_ltc_eur[ 'p' ][ 1 ] );
+  tick.number_trades = parseFloat( tick_ltc_eur[ 't' ][ 1 ] );
+  tick.ha_low = parseFloat( tick_ltc_eur[ 'l' ][ 1 ] );
+  tick.ha_high = parseFloat( tick_ltc_eur[ 'h' ][ 1 ] );
+  tick.opening_price = parseFloat( tick_ltc_eur[ 'o' ] );
+
+  return tick;
+}
+
+function saveData( tick ) {
+  Mongo.Db.connect( process.env.MONGOLAB_URI, function ( error, db ) {
+    if ( error ) {
+      console.log( 'Error connecting to database ' + error );
+    }
+    else {
+      db.collection( 'ticker', function ( error, collection ) {
+        collection.insert( tick, {safe: true}, function ( er, rs ) {
+          if ( er ) {
+            console.log( 'Error connecting to database ' + error );
+          }
+        } );
+      } );
+    }
+  } );
+}
