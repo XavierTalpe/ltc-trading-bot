5876d2f2552c523ccaf594573ea2304e8f2d8078
Xavier Talpe <xaviertalpe+pro@gmail.com>
2014-06-06 12:50:58 +0200

    Visualize ticker results.

--- routes/ticker.js
+++ routes/ticker.js
@@ -1,3 +1,42 @@
-/**
- * Created by xaviert on 5/27/14.
- */
+var Express = require( 'express' );
+var Mongo = require( 'mongodb' );
+var Async = require( 'async' );
+
+var Router = Express.Router();
+
+Router.get( '/', function ( request, response ) {
+  Async.waterfall( [
+                     function ( callback ) {
+                       Mongo.Db.connect( process.env.MONGOLAB_URI, callback );
+                     }                                                       ,
+                     function ( database, callback ) {
+                       database.collection( 'ticker', callback );
+                     }       ,
+                     function ( collection, callback ) {
+                       var minDate = new Date();
+                       minDate.setDate( minDate.getDate() - 1 );
+
+                       var query = {'datetime': {$gt: minDate.getTime()}};
+                       var options = {sort: [
+                         ['datetime', 'ascending']
+                       ]};
+
+                       collection.find( query, options ).toArray( callback );
+                     },
+                     function ( results, callback ) {
+                       callback( null, results );
+                     }
+                   ],
+                   function ( error, results ) {
+                     if ( error ) {
+                       response.send( error );
+                       response.status( 400 );
+                     }
+                     else {
+                       response.send( JSON.stringify( results ) );
+                       response.status( 200 );
+                     }
+                   } );
+} );
+
+module.exports = Router;
--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require( "express" );
 var logfmt = require( "logfmt" );
 
-var queryTicker = require( './routes/tasks/query_ticker' );
+var ticker = require( './routes/ticker' );
 
 var app = express();
 
+//app.engine( '.html', require( 'jade' ) );
 app.use( logfmt.requestLogger() );
 
-app.use( '/tasks/query-ticker', queryTicker );
-
-app.get( '/', function ( req, res ) {
-  res.send( 'Root!' );
-} );
+app.use( '/ticker', ticker );
+app.use( express.static( __dirname + '/public' ) );
 
 var port = Number( process.env.PORT || 5000 );
 app.listen( port, function () {
